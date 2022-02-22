33c09af1666e3cd61a4606c75087a92c5f7fb231
dmitry.pashkurov <dmitry.pahkurov@mail.ru>
2022-02-22 21:02:42 +0300

    добавил passportJs

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoDB: 'mongodb+srv://dmitry:UQcxHdTvbwhHfZXi@cluster0.azwmm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  mongoDB: 'mongodb+srv://dmitry:UQcxHdTvbwhHfZXi@cluster0.azwmm.mongodb.net/marketplace?retryWrites=true&w=majority',
+  jwt: 'marketplace-jwt'
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,65 @@
-module.exports.login = (req, res) => {
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password
-        }
+const UserModel = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys');
+
+module.exports.login = async (req, res) => {
+  const user = await UserModel.findOne({email: req.body.email});
+
+  if (user) {
+    const password = req.body.password;
+    const isCorrectPassword = bcrypt.compareSync(password, user.password);
+
+    if (isCorrectPassword) {
+
+      const token = jwt.sign({
+        email: user.email,
+        userId: user._id
+      }, keys.jwt, {expiresIn: 60 * 60})
+
+      res.status(200).json({
+        token: `Bearer ${token}`
+      })
+
+    } else {
+      res.status(401).json({
+        message: 'Password is incorrect'
+      })
+    }
+
+  } else {
+    res.status(404).json({
+      message: `User with email ${req.body.email} dont found`
     })
+  }
 }
 
-module.exports.register = (req, res) => {
-    res.status(200).json({
-        register: 'hello from controllers'
+module.exports.register = async (req, res) => {
+
+  const emailAlreadyUsed = await UserModel.findOne({email: req.body.email});
+
+  if(emailAlreadyUsed) {
+    res.status(409).json({
+      message: 'This email already used. For registration use another email'
     })
+  }
+
+  const salt = bcrypt.genSaltSync(10);
+  const password = req.body.password;
+
+  const user = new UserModel({
+    email: req.body.email,
+    password: bcrypt.hashSync(password, salt)
+  })
+
+  try {
+    await user.save();
+    res.status(201).json({
+      message: 'User successfully created',
+      user
+    })
+  } catch (e) {
+
+  }
+
 }
