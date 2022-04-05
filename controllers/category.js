5f611b967bed137b886facc83d984f3ac2e67bb3
dmitry.pashkurov <dmitry.pahkurov@mail.ru>
2022-04-05 21:00:02 +0300

    начал делать апи для категорий

--- controllers/category.js
+++ controllers/category.js
@@ -1,19 +1,56 @@
-module.exports.getAll = (req, res) => {
-  res.json({message: 'Categories response'})
+const errorHandler = require('../utils/errorHandler');
+const CategoryModel = require('../models/Category');
+const ProductModel = require('../models/Product');
+
+
+module.exports.getAll = async (req, res) => {
+  try {
+    const categories = CategoryModel.find({user: req.user.id})
+    res.status(200).json(categories);
+  } catch (e) {
+    errorHandler(res, e)
+  }
 };
 
-module.exports.getById = (req, res) => {
-
+module.exports.getById = async (req, res) => {
+  try {
+    const category = CategoryModel.findById(req.params.id);
+    res.status(200).json(category)
+  } catch (e) {
+    errorHandler(res, e)
+  }
 }
 
-module.exports.create = (req, res) => {
+module.exports.create = async (req, res) => {
+  const category = new CategoryModel({
+    name: req.body.name,
+    user: req.user.userId
+  })
 
+  try {
+    await category.save();
+    res.status(201).json(category);
+  } catch (e) {
+    errorHandler(res, e)
+  }
 }
 
-module.exports.remove = (req, res) => {
-
+module.exports.remove = async (req, res) => {
+  try {
+    await CategoryModel.remove({_id: req.params.id});
+    await ProductModel.remove({category: req.params.id});
+    res.status(200).json({
+      message: "Category successfully deleted"
+    })
+  } catch (e) {
+    errorHandler(res, e)
+  }
 }
 
-module.exports.update = (req, res) => {
+module.exports.update = async (req, res) => {
+  try {
 
+  } catch (e) {
+    errorHandler(res, e)
+  }
 }
--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const router = express.Router();
 const passport = require('passport');
 
-const controller = require('../controllers/category')
+const controller = require('../controllers/category');
+
+const router = express.Router();
 
 router.get('/', passport.authenticate('jwt', {session: false}), controller.getAll);
 router.get('/:id', controller.getById);
 router.delete('/:id', controller.remove);
+router.post('/', controller.create);
 router.patch('/:id', controller.update);
 
 
