4ecbdc18c5636b2a50d6f714815c24f8c330d641
dmitry.pashkurov <dmitry.pahkurov@mail.ru>
2022-04-05 20:59:38 +0300

    добавил апи для товаров

--- controllers/product.js
+++ controllers/product.js
@@ -15,7 +15,16 @@ module.exports.getByCategoryId = async (req, res) => {
 
 module.exports.create = async (req, res) => {
   try {
+    const newProduct = await new ProductModel({
+      name: req.body.name,
+      imgUrl: req.body.imgUrl,
+      description:  req.body.description,
+      price: req.body.price,
+      category: req.body.category,
+      user: req.user.id
+    }).save();
 
+    res.status(201).json(newProduct);
   } catch (e) {
     errorHandler(res, e)
   }
@@ -23,7 +32,12 @@ module.exports.create = async (req, res) => {
 
 module.exports.update = async (req, res) => {
   try {
-
+    const product = ProductModel.findOneAndUpdate(
+      {_id: req.body.id},
+      {$set: req.body},
+      {new: true}
+      );
+    res.status(200).json(product);
   } catch (e) {
     errorHandler(res, e)
   }
@@ -31,7 +45,10 @@ module.exports.update = async (req, res) => {
 
 module.exports.remove = async (req, res) => {
   try {
-
+    await ProductModel.remove({_id: req.params.id});
+    res.status(200).json({
+      message: `Position with ${req.params.id} `
+    })
   } catch (e) {
     errorHandler(res, e)
   }
